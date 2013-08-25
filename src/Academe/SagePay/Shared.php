3325fe88e39482a65a646ca283482399831af85f
Jason Judge <jason.judge@consil.co.uk>
2013-08-25 12:18:42 +0100

    Added shared/abort transaction type.

--- src/Academe/SagePay/Shared.php
+++ src/Academe/SagePay/Shared.php
@@ -16,22 +16,49 @@ class Shared extends Register //Common
 {
     /**
      * Release a DEFERRED or REPEATDEFERRED payment.
+     */
+
+    public function release($OriginalVendorTxCode = '', $VPSTxId = '', $SecurityKey = '', $TxAuthNo = 0, $Amount = 0)
+    {
+        return $this->abort_or_release('RELEASE', $OriginalVendorTxCode, $VPSTxId, $SecurityKey, $TxAuthNo, $Amount);
+    }
+
+    /**
+     * Abort or release a DEFERRED or REPEATDEFERRED payment.
+     */
+
+    protected function abort($TxType, $OriginalVendorTxCode = '', $VPSTxId = '', $SecurityKey = '', $TxAuthNo = 0)
+    {
+        return $this->abort_or_release('ABORT', $OriginalVendorTxCode, $VPSTxId, $SecurityKey, $TxAuthNo);
+    }
+
+    /**
+     * Abort or release a DEFERRED or REPEATDEFERRED payment.
      * The VendorTxCode in this case is not the primary key of the transaction. It now
      * refers to the key of the original deferred transaction. We probably need an OriginalVendorTxCode
      * for storage, but that gets sent to SagePay as simply VendorTxCode.
      * So pass the VendorTxCode in here, or set the OriginalVendorTxCode field on the transaction.
      */
 
-    public function release($VendorTxCode = '', $VPSTxId = '', $SecurityKey = '', $TxAuthNo = 0, $Amount = 0)
+    protected function abort_or_release($TxType, $OriginalVendorTxCode = '', $VPSTxId = '', $SecurityKey = '', $TxAuthNo = 0, $Amount = 0)
     {
+        if ($TxType == 'RELEASE') {
+            $message_type = 'shared-release';
+        } elseif ($TxType == 'ABORT') {
+            $message_type = 'shared-abort';
+        } else {
+            // Not a valid transaction type.
+            // TODO: throw error
+        }
+
         // Set the transaction type.
-        $this->setField('TxType', 'RELEASE');
+        $this->setField('TxType', $TxType);
 
         // The VendorTxCode passed in points to the eoriginal transaction being released, and is not
         // the ID of this transaction.
 
-        if ($VendorTxCode != '') {
-            $this->setField('OriginalVendorTxCode', $VendorTxCode);
+        if ($OriginalVendorTxCode != '') {
+            $this->setField('OriginalVendorTxCode', $OriginalVendorTxCode);
         } else {
             // If the VendorTxCode has been set (as the SagePay docs would lead) then move the
             // value to the Original field. This helps keep usability simple.
@@ -49,23 +76,27 @@ class Shared extends Register //Common
         if ($SecurityKey != '') $this->setField('SecurityKey', $SecurityKey);
         if ($TxAuthNo != 0) $this->setField('TxAuthNo', $TxAuthNo);
 
-        // The amount is put into ReleaseAmount to send to SagePay and Amount for storage.
+        // The Amount is only relevant for the RELEASE transaction.
 
-        if ($Amount != 0) {
-            $this->setAmount($Amount);
-        } else {
-            // Make sure the amount is in both places.
-            $amount = $this->getField('Amount');
-            if (empty($amount)) $this->setAmount($this->getField('ReleaseAmount'));
+        if ($TxType == 'RELEASE') {
+            // The amount is put into ReleaseAmount to send to SagePay and Amount for storage.
+
+            if ($Amount != 0) {
+                $this->setAmount($Amount);
+            } else {
+                // Make sure the amount is in both places.
+                $amount = $this->getField('Amount');
+                if (empty($amount)) $this->setAmount($this->getField('ReleaseAmount'));
+            }
+            $this->setField('ReleaseAmount', $this->getField('Amount'));
         }
-        $this->setField('ReleaseAmount', $this->getField('Amount'));
 
         // The fields that we will be dealing with.
         // These fields are all mandatory, so we should validate that all are set (TODO).
-        $fields = \Academe\SagePay\Metadata\Transaction::get('object', array('source' => 'shared-release'));
+        //$fields = \Academe\SagePay\Metadata\Transaction::get('object', array('source' => $message_type));
 
         // Construct the query string from data in the model.
-        $query_string = $this->queryData(true, 'shared-release');
+        $query_string = $this->queryData(true, $message_type);
 
         // Get the URL, which is derived from the method, platform and the service.
         $sagepay_url = $this->getUrl();
