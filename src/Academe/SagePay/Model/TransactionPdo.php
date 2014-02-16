b580282c7ea6c6cb0a72f3b837abfaadc8909cc0
Sami Greenbury <dude@patabugen.co.uk>
2014-02-16 15:27:08 +0000

    Made TransactionPDO SQLite compatible, and added a test

--- src/Academe/SagePay/Model/TransactionPdo.php
+++ src/Academe/SagePay/Model/TransactionPdo.php
@@ -83,7 +83,7 @@ class TransactionPdo extends TransactionAbstract
     }
 
     /**
-     * Save the transaction record to the database.
+     * Save the transaction record to storage.
      */
 
     public function save()
@@ -112,7 +112,10 @@ class TransactionPdo extends TransactionAbstract
                     'SELECT COUNT(*) FROM ' . $this->transaction_table_name
                         . ' WHERE VendorTxCode = :vendortxcode'
                 );
-                $stmt->bindParam(':vendortxcode', $this->getField('VendorTxCode'), \PDO::PARAM_STR);
+                // VendorTxCode is passed by reference, and we can only do this with
+                // variables (not function return values)
+                $vendorTxCode = $this->getField('VendorTxCode');
+                $stmt->bindParam(':vendortxcode', $vendorTxCode, \PDO::PARAM_STR);
 
                 $stmt->execute();
 
@@ -344,13 +347,19 @@ class TransactionPdo extends TransactionAbstract
 
             // Create the table.
 
-            $sql = 'CREATE TABLE IF NOT EXISTS ' . $this->transaction_table_name . " (\n";
+            $sql = 'CREATE TABLE IF NOT EXISTS `' . $this->transaction_table_name . "` (\n";
             $sql .= $this->createColumnsDdl();
-            $sql .= ', PRIMARY KEY pk_' . $this->transaction_table_name . ' (VendorTxCode) )';
+            if ($this->getDriver() == 'sqlite') {
+                $sql .= ', `pk_' . $this->transaction_table_name . '` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL'; 
+            } else {
+                $sql .= ', PRIMARY KEY `pk_' . $this->transaction_table_name . '` (`VendorTxCode`) '; 
+            }
+            $sql .= ')';
 
-
-            // TOOD: check the result.
             $success = $pdo->exec($sql);
+            if ($success === false) {
+                throw new Exception($pdo->errorInfo());
+            }
         }
         catch (\PDOException $e) {
             $this->pdo_error_message = $e->getMessage();
@@ -377,7 +386,7 @@ class TransactionPdo extends TransactionAbstract
         $columns = array();
 
         foreach($field_meta as $name => $field) {
-            $column = $name . ' ';
+            $column = '`'.$name . '` ';
 
             if (!isset($field->max)) continue;
 
@@ -460,6 +469,13 @@ class TransactionPdo extends TransactionAbstract
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
         return $pdo;
+    }
+
+    /**
+     * Returns the PDO driver name, e.g mysql or sqlite
+     */
+    protected function getDriver() {
+        return $this->getConnection()->getAttribute(\PDO::ATTR_DRIVER_NAME);
     }
 }
 
---
+++ tests/Academe/SagePay/Model/TransactionPdoSqliteTest.php
@@ -0,0 +1,55 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Academe\SagePay;
+
+class TransactionPdoSqliteTest extends PHPUnit_Framework_TestCase{
+
+    // For some reason SQLite fails with an in-memory database.
+
+    protected $dbFileName;
+    protected $server;
+    protected $storage;
+
+    protected function setUp(){
+        // SQLite does not apepar to be working with an in-memory database
+        // so we'll use a temporary file.
+        $this->dbFileName = tempnam('.', 'sqlitetest');
+        
+        $this->storage = new Academe\SagePay\Model\TransactionPdo();
+        $this->storage->setDatabase('sqlite:'.$this->dbFileName, null, null);
+
+        $this->server = new Academe\SagePay\Server();
+        $this->server->setTransactionModel($this->storage);
+    }
+
+    protected function tearDown(){
+        unlink($this->dbFileName);
+    }
+
+    public function testCreateTable(){
+        $success = $this->storage->createTable();
+        $this->assertTrue($success);
+    }
+
+    public function testCreateTransaction(){
+        $this->storage->createTable();
+        
+        $this->server->setMain('PAYMENT', 'vendorx', '99.99', 'GBP', 'Store purchase', 'http://example.com/mycallback.php');
+        $success = $this->server->save();
+        $this->assertTrue($success);
+    }
+
+    public function testUpdateTransaction(){
+        $this->storage->createTable();
+        // Create a model
+        $this->server->setMain('PAYMENT', 'vendorx', '99.99', 'GBP', 'Store purchase', 'http://example.com/mycallback.php');
+        $this->server->save();
+
+        // Now change the model, and check we can update it
+        $this->server->setField('Amount', '10');
+        $success = $this->server->save();
+        $this->assertTrue($success);
+    }
+}
